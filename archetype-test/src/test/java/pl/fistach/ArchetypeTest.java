cb45da05e3e1cba5863a9069e9374f079bd32b9a
fistach <fistach@gmail.com>
2025-11-02 20:51:53 +0100

    add module for archetype tests

---
+++ archetype-test/src/test/java/pl/fistach/ArchetypeTest.java
@@ -0,0 +1,13 @@
+package pl.fistach;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class ArchetypeTest
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
