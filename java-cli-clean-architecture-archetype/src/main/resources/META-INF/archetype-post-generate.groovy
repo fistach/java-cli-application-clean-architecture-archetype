6132782695ca85e8fcc3d033ef1b7c7ca070c481
fistach <fistach@gmail.com>
2025-11-30 22:33:29 +0100

    remove post generate script

--- java-cli-clean-architecture-archetype/src/main/resources/META-INF/archetype-post-generate.groovy
+++
@@ -1,5 +0,0 @@
-def groupId = binding.variables['groupId']
-
-if (groupId == 'test') {
-    throw new RuntimeException("Uzyto groupId o wartości 'test'. To powoduje błąd generowania projektu. Wybierz inną wartość.")
-}
