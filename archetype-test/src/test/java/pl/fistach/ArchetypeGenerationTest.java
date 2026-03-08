341684d2d84fd442701c01a051de401357754daa
fistach <fistach@gmail.com>
2026-03-08 17:32:46 +0100

    improve tests

--- archetype-test/src/test/java/pl/fistach/ArchetypeGenerationTest.java
+++ archetype-test/src/test/java/pl/fistach/ArchetypeGenerationTest.java
@@ -12,12 +12,15 @@ import org.junit.jupiter.api.Test;
 import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class ArchetypeGenerationTest {
 
+    public static final String ARTIFACT_ID = "calculator";
     private static final Path ROOT =
-            Path.of("target", "it", "generate-archetype-test", "calculator");
+            Path.of("target", "it", "generate-archetype-test", ARTIFACT_ID);
 
     private static Model model;
 
@@ -39,33 +42,45 @@ class ArchetypeGenerationTest {
 
     @Test
     void projectShouldContainModules() {
-        assertFalse(model.getModules().isEmpty(),
+        //when
+        List<String> modules = model.getModules();
+
+        //then
+        assertFalse(modules.isEmpty(),
                 "Project should contain modules, but <modules> list is empty");
 
-        for (String module : model.getModules()) {
-            Path moduleDir = ROOT.resolve(module);
+        modules.forEach(it -> {
+            Path moduleDir = ROOT.resolve(it);
             assertTrue(Files.exists(moduleDir),
                     "module dir doesn't exist: " + moduleDir);
-        }
+        });
+
+        Set<String> expectedModules = Set.of(ARTIFACT_ID + "-main", ARTIFACT_ID + "-controller");
+        assertEquals(expectedModules, new HashSet<>(modules));
     }
 
     @Test
     void projectShouldContainOneControllerModule() {
+        //given
         String rootArtifactId = model.getArtifactId();
         List<String> modules = model.getModules();
 
         int controllerCount = 0;
 
+        //when
         for (String module : modules) {
             if (module.equals(rootArtifactId + "-controller")) {
                 controllerCount++;
             }
         }
+
+        //then
         assertEquals(1, controllerCount, "Project should contain exactly one 'controller' module");
     }
 
     @Test
     void mainModuleShouldDependOnController() throws Exception {
+        //given
         String rootArtifactId = model.getArtifactId();
         String expectedControllerArtifact = rootArtifactId + "-controller";
 
@@ -73,13 +88,16 @@ class ArchetypeGenerationTest {
         MavenXpp3Reader reader = new MavenXpp3Reader();
         Model mainModel = reader.read(new FileReader(mainModulePomPath.toFile()));
 
+        //when
         long controllerDependenciesCount = mainModel.getDependencies()
                 .stream()
                 .map(Dependency::getArtifactId)
                 .filter(expectedControllerArtifact::equals)
                 .count();
-
+        //then
         assertEquals(1, controllerDependenciesCount,
                 "Module 'main' should depend exactly once on " + expectedControllerArtifact);
     }
+
+
 }
